c4e098f652b410ac452587cecd9c31fb6df05809
abdrah124 <abdmanharits@gmail.com>
2024-03-18 16:54:44 +0700

    Fix using metadata in client component

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,91 +1,11 @@
-"use client";
-import CoverImage from "@/components/image/cover-image";
-import PostsGridLayout from "@/components/layout/posts-grid-layout";
-import PostCard from "@/components/post/post-card";
-import ProfileImage from "@/components/image/profile-image";
-import { TypographyH3, TypographyMuted } from "@/components/ui/typography";
-import { Divider } from "@nextui-org/divider";
-import React from "react";
-import ProfileInfo from "@/components/profile/profile-info";
-import { useGetMyAccountInfo, useGetMyPosts } from "@/lib/api/account/query";
-import ProfileActionButton from "@/components/profile/profile-action-button";
-import CreatePostForm from "@/components/form/create-post-form";
-import ProfileSkeleton from "@/components/profile/profile-skeleton";
-import PostCardSkeleton from "@/components/post/skeleton";
-import useFetchNextPageObserver from "@/hooks/use-fetch-next-page";
-import { Spinner } from "@nextui-org/spinner";
-import TextWithLimit from "@/components/text-with-limit";
 import { Metadata } from "next";
+import ProfilePage from "@/components/page/profile-page";
 
 export const metadata: Metadata = {
   title: "Profile",
   description: "My profile page",
 };
 
-export default function ProfilePage() {
-  const { resp, isLoading, isSuccess } = useGetMyAccountInfo();
-  const {
-    myPosts,
-    isLoading: isPostLoading,
-    isSuccess: isPostSuccess,
-    isFetching,
-    isFetchNextNotAvailable,
-    fetchNextPage,
-    isFetchingNextPage,
-  } = useGetMyPosts();
-
-  const { ref } = useFetchNextPageObserver({
-    fetchNextPage,
-    isDisabled: isFetchNextNotAvailable,
-    isFetching,
-  });
-
-  const userPosts = myPosts?.data ?? [];
-  const followers = resp?.data?.count?.followedBy ?? 0;
-  const following = resp?.data?.count?.following ?? 0;
-  const totalPost = resp?.data?.count?.posts ?? 0;
-
-  return (
-    <>
-      {isLoading ? (
-        <ProfileSkeleton />
-      ) : (
-        isSuccess && (
-          <div className="w-full pt-2 flex flex-col items-center justify-start gap-2 relative">
-            <CoverImage src={resp?.data?.profile?.coverImage?.src ?? ""} />
-            <ProfileImage src={resp?.data?.profile?.avatarImage?.src ?? ""} />
-            <div className="flex pt-20 md:pt-16 flex-col justify-start items-center w-full px-4 md:items-start md:px-6 md:pb-2">
-              <div className="flex gap-2 flex-col md:items-start justify-start md:flex-shrink-0 max-w-full">
-                <TypographyH3 className="text-center">
-                  {resp?.data?.fullName}
-                </TypographyH3>
-                <TypographyMuted className="text-center">
-                  {resp?.data?.username}
-                </TypographyMuted>
-                <TextWithLimit text={resp?.data?.profile?.description ?? ""} />
-              </div>
-
-              <ProfileInfo
-                userId={resp?.data?.id ?? -1}
-                followedCount={following}
-                followersCount={followers}
-                postCount={totalPost}
-              />
-            </div>
-            <ProfileActionButton />
-          </div>
-        )
-      )}
-      <Divider className="mt-7" />
-      <CreatePostForm isNotPostPage withPreview={false} autoFocus={false} />
-      <PostsGridLayout>
-        {isPostLoading
-          ? [1, 2, 3, 4, 5].map((item) => <PostCardSkeleton key={item} />)
-          : isPostSuccess &&
-            userPosts?.map((post) => <PostCard key={post?.id} post={post} />)}
-        {isFetchingNextPage && <Spinner className="mt-4" />}
-        <div className="w-full" ref={ref}></div>
-      </PostsGridLayout>
-    </>
-  );
+export default function Page() {
+  return <ProfilePage />;
 }
--- app/(main)/users/[userId]/page.tsx
+++ app/(main)/users/[userId]/page.tsx
@@ -1,25 +1,6 @@
-"use client";
-import CoverImage from "@/components/image/cover-image";
-import ProfileImage from "@/components/image/profile-image";
-import PostsGridLayout from "@/components/layout/posts-grid-layout";
-import PostCard from "@/components/post/post-card";
-import PostCardSkeleton from "@/components/post/skeleton";
-import ProfileInfo from "@/components/profile/profile-info";
-import ProfileSkeleton from "@/components/profile/profile-skeleton";
-import TextWithLimit from "@/components/text-with-limit";
-import { TypographyH3, TypographyMuted } from "@/components/ui/typography";
-import UserActionButton from "@/components/user/user-action-button";
-import useFetchNextPageObserver from "@/hooks/use-fetch-next-page";
-import { useNotFoundRedirect } from "@/hooks/use-not-found-redirect";
-import { useGetPostByUserId } from "@/lib/api/posts/query";
-import { useGetUserById } from "@/lib/api/users/query";
-import { useSession } from "@/stores/auth-store";
-import { Post } from "@/types/post";
-import { Divider } from "@nextui-org/divider";
-import { Spinner } from "@nextui-org/spinner";
+import UserPage from "@/components/page/users-page";
 import { Metadata } from "next";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React from "react";
 
 export function generateMetadata({ params }: any): Metadata {
   return {
@@ -30,85 +11,6 @@ export function generateMetadata({ params }: any): Metadata {
   };
 }
 
-export default function UserPage({ params }: { params: { userId: string } }) {
-  const router = useRouter();
-  const session = useSession();
-
-  useEffect(() => {
-    if (session?.id === Number(params.userId)) router.replace("/profile");
-  }, [session]);
-
-  const { userData, isLoading, isSuccess, isError, error } = useGetUserById(
-    Number(params?.userId)
-  );
-  const {
-    posts,
-    isLoading: isPostsLoad,
-    isSuccess: isPostsSuccess,
-    fetchNextPage,
-    isFetching,
-    isFetchingNextPage,
-    isFetchNextNotAvailable,
-  } = useGetPostByUserId(Number(params.userId));
-
-  useNotFoundRedirect(error, isError);
-
-  const { ref } = useFetchNextPageObserver({
-    isDisabled: isFetchNextNotAvailable || isError,
-    fetchNextPage,
-    isFetching,
-  });
-
-  return (
-    <>
-      {isLoading ? (
-        <ProfileSkeleton />
-      ) : (
-        isSuccess && (
-          <div className="w-full pt-2 flex flex-col items-center justify-start gap-2 relative">
-            <CoverImage
-              src={userData?.data?.profile?.coverImage?.src ?? ""}
-              className="pt-2"
-            />
-            <ProfileImage
-              isNotOwned
-              src={userData?.data?.profile?.avatarImage?.src ?? ""}
-            />
-            <div className="flex pt-20 md:pt-16 flex-col justify-start items-center w-full px-4 md:items-start md:px-6 md:pb-2">
-              <div className="flex gap-2 flex-col md:items-start justify-start md:flex-shrink-0 max-w-full break-words">
-                <TypographyH3 className="text-center">
-                  {userData?.data?.fullName ?? ""}
-                </TypographyH3>
-                <TypographyMuted className="text-center md:!text-base">
-                  {userData?.data?.username ?? ""}
-                </TypographyMuted>
-                <TextWithLimit
-                  text={userData?.data?.profile?.description ?? ""}
-                />
-              </div>
-
-              <ProfileInfo
-                userId={Number(params.userId)}
-                postCount={posts?.pagination?.totalRecords ?? 0}
-                followedCount={userData?.data?.count?.following ?? 0}
-                followersCount={userData?.data?.count?.followedBy ?? 0}
-              />
-            </div>
-            <UserActionButton userId={userData?.data?.id ?? -1} />
-          </div>
-        )
-      )}
-      <Divider className="mt-7" />
-      <PostsGridLayout>
-        {isPostsLoad
-          ? [1, 2].map((item) => <PostCardSkeleton key={item} />)
-          : isPostsSuccess &&
-            posts?.data?.map((data: Post) => (
-              <PostCard post={data} key={data?.id} />
-            ))}
-        {isFetchingNextPage && <Spinner className="mt-4" />}
-        <div ref={ref} className="w-full"></div>
-      </PostsGridLayout>
-    </>
-  );
+export default function Page({ params }: { params: { userId: string } }) {
+  return <UserPage params={params} />;
 }
